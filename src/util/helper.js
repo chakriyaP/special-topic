b1e59a5d24eb9cfe2e6721eae146ba4f30826a01
ZShiroeSamaZ <47985474+ZShiroeSamaZ@users.noreply.github.com>
2022-03-22 23:48:58 +0700

    add docker url

---
+++ src/util/helper.js
@@ -0,0 +1,4 @@
+const baseUrl = "https://asia-southeast1-kku-smart-farm.cloudfunctions.net/api";
+const dockerUrl = "http://localhost:5001/kku-smart-farm/asia-southeast1/api";
+
+export { baseUrl, dockerUrl };
