bc619d6473d5da13fca15a9c9c7412e580e12570
phanthakan <phanthakan@kkumail.com>
2022-04-18 18:59:51 +0700

    aetting Time

--- src/util/helper.js
+++ src/util/helper.js
@@ -1,5 +1,6 @@
 const baseUrl = "https://asia-southeast1-kku-smart-farm.cloudfunctions.net/api";
-const dockerUrl = "http://localhost:5001/kku-smart-farm/asia-southeast1/api";
-const boardId = "1.24.155.12"
+const dockerUrl =
+  "https://a4a8-202-12-97-141.ngrok.io/kku-smart-farm/asia-southeast1/api";
+const boardId = "1.24.155.12";
 
 export { baseUrl, dockerUrl, boardId };
